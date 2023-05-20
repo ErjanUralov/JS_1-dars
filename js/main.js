f9622ed0fab0c58521197df36c1888a06e05ffae
ErjanUralov <uraloverjan777@gmail.com>
2023-05-20 09:24:29 +0500

    Ushbu repositoryda JS 1-dars kodlari bor.
    JS 1-dasr kodlari yuklandi

---
+++ js/main.js
@@ -0,0 +1,210 @@
+/* console.log(4+4); /*yozuvni console oynasida chiqaradi*/
+/* alert("Saytimizga xush kelibsiz"); /*birinci bolib tepadan tuwib keladi*/
+
+/* let x=2;  variable = o`zgaruvchi */
+/* let y=5;
+let salom = "Assalomu aleykum"
+document.write(salom);*/
+
+/* JSda arifmetik hisoblashlar*/
+/* let soz ="javascript" */  /* data type: string */
+/* let x=5;  */              /* data type: number*/
+/* x+=5; */  /* x=5+5 */
+/* x-=2; */  /* x=5-2 */
+/* x*=2; */  /* x=5*2 */
+/* x/=2; */  /* x=5/2 */
+/* let qoldiq = x%5;  */   /*qoldiq chiqarish */
+/* let natija = x*5;  */   /*ko`paytirish */
+/* ++x; */                 /* 1+ qlib beradi*/
+/* --x; */                 /* -1 qlib beradi*/
+/* x++; */
+/* x--; */
+/* document.write(x+5) */
+
+/* if => agar */
+/* else if => yoki bo`lmasa */
+/* else => holda */
+
+/*if ("salom" == "salom"){
+    console.log("Ular teng!");
+}
+else {
+    console.log("ular teng emes!")
+}*/
+
+/*if (45==46){
+    console.log("bu son 45 ga teng")
+}
+
+else if (45==44){
+    console.log("Endi bu son 45ga teng")
+}
+else  {
+    console.log("ikala son ham 45ga teng emas!")
+}*/
+
+/*
+if ("7" < "5"){
+    console.log("7 katta 5 dan");
+}
+else {
+    console.log("5 kichik 7dan")
+}
+
+if ("77 yosh" == "59 yosh"){
+    console.log("Ular teng!");
+}
+else {
+    console.log("ular teng emes!")
+}
+
+if ("aka " >= "Uka"){
+    console.log("Togri aka katta!");
+}
+else {
+    console.log("Ukasi katta! ")
+}
+
+if (125 <= 130){
+    console.log("True");
+}
+else {
+    console.log("No is correct")
+}
+
+if (160 <= 130){
+    console.log("True");
+}
+else {
+    console.log("No is correct")
+}
+
+
+if (45==46){
+    console.log("bu son 45 ga teng")
+}
+
+else if (45==44){
+    console.log("Endi bu son 45ga teng")
+}
+else  {
+    console.log("ikala son ham 45ga teng emas!")
+}
+
+if (45 >= 46){
+    console.log(" Bu son 45 dan katta!")
+}
+
+else if (47+55 > 55+48){
+    console.log("Endi bu son 47ga teng")
+}
+else  {
+    console.log(" Bu ikiham  49dan kichik ")
+}
+
+if ("Nodir"=="Qodir"){
+    console.log("Ular dost emas")
+}
+
+else if (45==44){
+    console.log("Ular sinfdoshlar")
+}
+else  {
+    console.log("Ular qadirdon do`stlar!")
+}
+
+if (77 <=77){
+    console.log("Ha bu To`g`ri")
+}
+
+else if (77==77){
+    console.log("Ular teng!")
+}
+else  {
+    console.log("Xato!")
+}
+
+
+
+if (898<=788){
+    console.log("Togri")
+}
+else if (898 < 778){
+    console.log("Bu togri javob")
+}
+else  {
+    console.log("Yo`q ikkisi ham xato javoblar!")
+}
+*/
+
+/*
+let x=2, y=5;
+
+if (x*5 <= y+4){
+    console.log("Togri")
+}
+
+else if (10 < 9){
+    console.log("ha endi togri!")
+}
+else  {
+    console.log("Bu amal noto`g`ri bajarildi!")
+}*/
+
+/*
+let g="men IT ni organyapman", t="Men kampyuterni bilaman"
+
+if (g == t){
+    console.log("U IT-park talabasi");
+}
+else if (g <= t){
+    console.log("U Asturnda talim oladi")
+}
+else {
+    console.log("Men IT-parkning yakuning bosqich talabasiman")
+}*/
+
+/*
+
+let q="52", p="88"
+
+if (q <= p){
+    console.log("To`g`ri javob 88 raqami");
+}
+else if (q >= p){
+    console.log("")
+}
+else {
+    console.log("bilmayman")
+}
+*/
+
+/*
+let z=22, c=55
+
+if (z+"raqamli oyinchi  yaxshi bola" >= c+"raqamli oyinchi esa eng chaqqoni"  ){
+    console.log("Bu ikkalasi ham juda wox bolalar!");
+}
+else if (z <= c){
+    console.log("Bu ikkalasi ham juda yaxshi bolalar!")
+}
+else {
+    console.log("bu bolalar ota tarbiyali bolalardir!!!")
+}
+*/
+
+/*
+let m=22, n=55, b=78, w=41, r=92;
+
+if (m+n+b == w+r){
+    console.log("Yoq bu xato");
+}
+else if (z > c){
+    console.log("ular teng!")
+}
+else {
+    console.log("Bu sonlar Yigindilari bir-biriga teng!!!")
+}
+*/
+
+
